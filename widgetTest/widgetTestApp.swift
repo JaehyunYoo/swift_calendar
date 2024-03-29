6fed9d84fa2c25fa920a6d453c6c22703adb5744
JaeHyunYoo <nike361@naver.com>
2024-03-29 18:21:55 +0900

    Initial Commit

---
+++ widgetTest/widgetTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  widgetTestApp.swift
+//  widgetTest
+//
+//  Created by air on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct widgetTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
